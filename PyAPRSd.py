391f964f6cdd381bf819414c297f2420ce4e11b1
Andy Smith <andy@ve7cxz.net>
2014-06-04 19:03:04 -0700

    Removed legitimate callsign and APRS passcode... oops

--- PyAPRSd.py
+++ PyAPRSd.py
@@ -9,8 +9,5 @@ config.read('PyAPRSd.cfg')
 #client = aprs.Client()
 #client.connect('vancouver.aprs2.net', 14580)
 core = aprs.Core()
-core.add_client('vancouver.aprs2.net', 14580, 'VE7CXZ', '19134', 'r/48.45/-123.4/20')
-core.add_client('noam.aprs2.net', 14580, 'VE7CXZ', '19134', 'r/48.45/-123.4/20')
 core.add_manager(54321, "admin", "password")
-print "stuff"
 core.start()
